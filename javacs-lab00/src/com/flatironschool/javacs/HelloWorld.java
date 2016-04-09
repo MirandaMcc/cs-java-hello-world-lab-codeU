a01885061ec181f17a3f1b80621ccd3c78546a53
Miranda <mcclm@mit.edu>
2016-04-09 17:44:58 -0400

    Updated getVersion

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,11 +2,16 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        return Double.parseDouble(System.getProperty("java.version"));
+		//java version "1.8.0_60"
+		String version = System.getProperty("java.version");
+		String sub = version.substring(0,3);
+		System.out.println(version);
+		System.out.println(sub);
+        return Double.parseDouble(sub);
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
-		System.out.println(getVersion());
+		getVersion();
     }
 }
